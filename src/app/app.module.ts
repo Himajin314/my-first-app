c3b411dc5fff6d78dac96494c772847ec196d173
yoshihiko sugiyama <j7ca5xy2p6@outlook.jp>
2021-12-27 01:35:37 +0900

    Productコンポーネントをモジュール化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListComponent } from './common/product-list/product-list.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent,
   ],
   imports: [
     BrowserModule,
