d08c6bf79ed482ee03d1ce8629c3b7bd671e0c80
yoshihiko sugiyama <j7ca5xy2p6@outlook.jp>
2022-01-13 23:16:12 +0900

    dev

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //DB_URI: 'mongodb+srv://yoshihiko_sugiyama:urUIovoRFpueOi9C@cluster0.cwpzj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    DB_URI:'mongodb+srv://yoshihiko_sugiyama:dmuEE7wqa4vTiy1k@cluster0.h64vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+//このファイルは絶対にgithubに登録しない .gitignoreに必ず使用する
+//mongodb+srv://yoshihiko_sugiyama:dmuEE7wqa4vTiy1k@cluster0.h64vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
